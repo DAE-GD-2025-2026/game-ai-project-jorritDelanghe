d958dccb54dc2cc48b82b5c44258dff58fa088ad
jorritDelanghe <157893404+jorritDelanghe@users.noreply.github.com>
2026-03-16 10:13:04 +0100

    BFS
    implemented path finding for BFS

--- Source/GameAIProg/GraphTheory/Algorithms/BFS.cpp
+++ Source/GameAIProg/GraphTheory/Algorithms/BFS.cpp
@@ -16,5 +16,41 @@ BFS::BFS(Graph* const pGraph)
 std::vector<Node*> BFS::FindPath(Node* const pStartNode, Node* const pDestinationNode) const
 {
 	std::vector<Node*> path;
+	std::queue<Node*> queue;
+	std::map<Node*, Node*> nodeMap; //key is the child, value = parent
+	Node* currentNode{};
+	
+	queue.push(pStartNode);
+	while (!queue.empty())
+	{
+		currentNode = queue.front();
+		queue.pop();
+		
+		if (currentNode == pDestinationNode)
+		{
+			break;
+		}
+		auto connections = pGraph->FindConnectionsFrom(currentNode->GetId());
+		
+		for (auto* connection: connections)
+		{
+			Node* neighbourNode = pGraph->GetNode(connection->GetToId()).get();
+			if (nodeMap.find(neighbourNode) == nodeMap.end())
+			{
+				queue.push(neighbourNode);
+				nodeMap.emplace(neighbourNode, currentNode);
+			}
+		}
+	}
+	if (nodeMap.find(pDestinationNode) != nodeMap.end())
+	{
+		currentNode = pDestinationNode;
+		while (currentNode!= nullptr)
+		{
+			path.push_back(currentNode);
+			currentNode = nodeMap[currentNode];
+		}
+	}
+	std::reverse(path.begin(), path.end());
 	return path;
 }
