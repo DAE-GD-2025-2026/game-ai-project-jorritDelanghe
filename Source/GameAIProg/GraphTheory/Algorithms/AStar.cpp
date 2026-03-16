76bbbb9476b32f3aa91490cec3636d6a8e3e6f62
jorritDelanghe <157893404+jorritDelanghe@users.noreply.github.com>
2026-03-16 12:11:21 +0100

    A star
    implemented the path finding for A star

--- Source/GameAIProg/GraphTheory/Algorithms/AStar.cpp
+++ Source/GameAIProg/GraphTheory/Algorithms/AStar.cpp
@@ -1,5 +1,7 @@
 ﻿#include "AStar.h"
 
+#include <queue>
+
 using namespace GameAI;
 
 AStar::AStar(Graph* const pGraph, HeuristicFunctions::Heuristic hFunction)
@@ -11,6 +13,97 @@ AStar::AStar(Graph* const pGraph, HeuristicFunctions::Heuristic hFunction)
 std::vector<Node*>AStar::FindPath(Node* const pStartNode, Node* const pGoalNode)
 {
 	std::vector<Node*> path{};
+	NodeRecord startRecord
+	{pStartNode
+		,nullptr
+		,0.f
+		,GetHeuristicCost(pStartNode, pGoalNode)
+	};
+	
+    std::vector<NodeRecord> openList{};
+	std::vector<NodeRecord> closedList{};
+	
+	openList.push_back(startRecord);
+	NodeRecord currentRecord{};
+	
+	while (!openList.empty())
+	{
+		currentRecord = *std::min_element(openList.begin(), openList.end());
+	
+		if (currentRecord.pNode == pGoalNode)
+		{
+			break;
+		}
+		
+		auto connections = pGraph->FindConnectionsFrom(currentRecord.pNode->GetId());
+		for (auto* connection : connections)
+		{
+			Node* pNextNode = pGraph->GetNode(connection->GetToId()).get();
+			const float newG{currentRecord.costSoFar + connection->GetWeight()};
+			
+			auto closedIt= std::find_if(
+				closedList.begin(),closedList.end(),
+				[pNextNode](const NodeRecord& closedRecord)
+				{
+					return pNextNode == closedRecord.pNode;
+				});
+			if (closedIt != closedList.end())
+			{
+				if (closedIt->costSoFar<=newG)
+				{
+					continue; //existing record is cheaper, skip
+				}
+				closedList.erase(closedIt);
+			}
+			
+			auto openIt = std::find_if(
+				openList.begin(), openList.end()
+				,[pNextNode](const NodeRecord& openRecord)
+				{
+					return pNextNode == openRecord.pNode;
+				});
+			if (openIt != openList.end())
+			{
+				if (openIt->costSoFar<=newG)
+				{
+					continue;
+				}
+				openList.erase(openIt);
+			}
+			NodeRecord newRecord{
+				pNextNode
+				,connection
+				,newG
+				, newG+ GetHeuristicCost(pNextNode,pGoalNode)
+				
+			};
+			openList.push_back(newRecord);
+		}
+			openList.erase(std::find(
+				openList.begin(), 
+				openList.end(), 
+				currentRecord));
+			closedList.push_back(currentRecord);
+	}
+	
+	if (currentRecord.pNode == pGoalNode)
+	{
+		while (currentRecord.pNode != pStartNode)
+		{
+			path.push_back(currentRecord.pNode);
+			int fromId = currentRecord.pConnection->GetFromId();
+			Node* pFromNode = pGraph->GetNode(fromId).get();
+			currentRecord =  *std::find_if(
+				closedList.begin()
+				,closedList.end()
+				,[pFromNode](const NodeRecord& closedRecord)
+				{
+					return pFromNode == closedRecord.pNode;
+				});
+		}
+		path.push_back(pStartNode);
+		std::reverse(path.begin(), path.end());
+	}
 	return path;
 }
 
