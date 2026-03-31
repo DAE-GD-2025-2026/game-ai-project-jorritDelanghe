e5aa742bb6092e160c0c97885860767ad7b9f305
jorritDelanghe <157893404+jorritDelanghe@users.noreply.github.com>
2026-03-31 18:42:11 +0200

    create navigation graph

--- Source/GameAIProg/Shared/Graph/NavGraph/NavGraph.cpp
+++ Source/GameAIProg/Shared/Graph/NavGraph/NavGraph.cpp
@@ -48,12 +48,54 @@ int GameAI::NavGraph::GetNodeIdFromEdgeIndex(int EdgeIdx) const
 
 void GameAI::NavGraph::CreateNavigationGraph()
 {
-	//1. Go over all the edges of the navigation mesh and create nodes
-			// Create node here
-
-	//2. Create connections now that every node is created	
+	//1. Goes over all the edges of the navigation mesh and creates nodes
+ const auto& edges = pNavPoly->GetEdges();
+	const auto& triangles = pNavPoly->GetTriangles();
+	
+	for (size_t i{}; i <edges.size();++i)
+	{
+		int sharedEdge{0};
+		for (const auto& triangle:triangles)
+		{
+			if (triangle.HasEdge(edges[i])) ++sharedEdge;
+		}
+		if (sharedEdge == 2) //portal
+		{
+			const FVector p1{edges[i].GetP1(*pNavPoly)};
+			const FVector p2{edges[i].GetP2(*pNavPoly)};
+			FVector2D middlePoint{(p1.X + p2.X)/2.f, (p1.Y + p2.Y/2.f)};
+			AddNode(std::make_unique<NavGraphNode>(middlePoint,i));
+		}
+	}
+	//2. Creates connections 
 		//2 valid nodes -> 1 connection
 		//3 valid nodes -> 3 connections
+	
+	for (const auto& triangle:triangles)
+	{
+		std::vector<int> validIds{};
+		for (const auto& edge: triangle.GetEdges())
+		{
+			auto edgeIndex = pNavPoly->FindEdgeIndex(edge);
+			if (!edgeIndex.has_value()) continue;
+			int nodeId{GetNodeIdFromEdgeIndex(edgeIndex.value())};
+			if (nodeId != Graphs::InvalidNodeId)
+			{
+				validIds.push_back(nodeId);
+			}
+		}
 		
+		if (validIds.size()==2)
+		{
+			AddConnection(validIds[0], validIds[1]);
+		}
+		else if (validIds.size()==3)
+		{
+			AddConnection(validIds[0], validIds[1]);
+			AddConnection(validIds[1], validIds[2]);
+			AddConnection(validIds[0], validIds[2]);
+		}
+	}
 	//3. Set the connections cost to the actual distance
+	SetConnectionCostsToDistances();
 }
