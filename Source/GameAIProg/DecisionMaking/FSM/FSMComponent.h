d9ebdafb53fa7ce2e27078ff2f74ce6ff907d438
jorritDelanghe <157893404+jorritDelanghe@users.noreply.github.com>
2026-04-27 14:29:05 +0200

    added comments + getBlackboard function

--- Source/GameAIProg/DecisionMaking/FSM/FSMComponent.h
+++ Source/GameAIProg/DecisionMaking/FSM/FSMComponent.h
@@ -8,13 +8,7 @@
 #include "CoreMinimal.h"
 #include "BrainComponent.h"
 #include "FSMComponent.generated.h"
-
-namespace GameAI::FSM
-{
-	class State;
-	class Transition;
-	class FSM; // contains FSM logic
-}
+#include "FSM.h"
 
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
 class GAMEAIPROG_API UFSMComponent : public UBrainComponent
@@ -34,14 +28,17 @@ public:
 	
 	virtual bool IsRunning() const override; 
 	
+	// Add a state — forwards ownership to the FSM
 	void AddState(std::unique_ptr<GameAI::FSM::State>&& NewState);
+	// Add a transition between two states
 	void AddTransition(GameAI::FSM::State* From, GameAI::FSM::State* To, std::function<bool()> EvalFunc) const;
-		
+	// Get the blackboard from the owning AIController
+	UBlackboardComponent* GetBlackBoardComponent() const;
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
 private:
-	//std::unique_ptr<GameAI::FSM::FSM> FSMInstance;
+	std::unique_ptr<GameAI::FSM::FSM> FSMInstance;
 	bool bIsRunning{false};
 };
