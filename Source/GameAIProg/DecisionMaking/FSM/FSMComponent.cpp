f7ccbbcd572f97edbecda68037b7713fec7f3af5
jorritDelanghe <157893404+jorritDelanghe@users.noreply.github.com>
2026-04-27 14:38:39 +0200

    FSMComponent

--- Source/GameAIProg/DecisionMaking/FSM/FSMComponent.cpp
+++ Source/GameAIProg/DecisionMaking/FSM/FSMComponent.cpp
@@ -3,6 +3,8 @@
 
 #include "FSMComponent.h"
 
+#include "AIController.h"
+
 
 // Sets default values for this component's properties
 UFSMComponent::UFSMComponent()
@@ -10,19 +12,27 @@ UFSMComponent::UFSMComponent()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
-	// TODO Setup FSM
+	FSMInstance = std::make_unique<GameAI::FSM::FSM>();
 }
 
 
 void UFSMComponent::AddState(std::unique_ptr<GameAI::FSM::State>&& NewState)
 {
-	// TODO
+		if (FSMInstance) FSMInstance->AddState(std::move(NewState));
 }
 
 void UFSMComponent::AddTransition(GameAI::FSM::State* From, GameAI::FSM::State* To, std::function<bool()> EvalFunc) const
 {
-	// TODO
+	if (FSMInstance) FSMInstance->AddTransition(From,To, std::move(EvalFunc));
+}
+
+UBlackboardComponent* UFSMComponent::GetBlackBoardComponent() const
+{
+	if (AAIController* AIController = Cast<AAIController>(GetOwner()))
+	{
+		return AIController->GetBlackboardComponent();
+	}
+	return nullptr;
 }
 
 // Called when the game starts
@@ -36,19 +46,30 @@ void UFSMComponent::BeginPlay()
 void UFSMComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-	// TODO
+	if (bIsRunning && FSMInstance)
+	{
+		FSMInstance->Update(DeltaTime, GetBlackBoardComponent());
+	}
 }
 
 void UFSMComponent::StartLogic()
 {
 	Super::StartLogic();
 
-	// TODO
+	if (FSMInstance)
+	{
+		bIsRunning = true;
+		FSMInstance->Start(GetBlackBoardComponent());
+	}
 }
 
 void UFSMComponent::StopLogic(const FString& Reason)
 {
-	// TODO
+	if (FSMInstance)
+	{
+		FSMInstance->Stop(GetBlackBoardComponent());
+		bIsRunning = false;
+	}
 }
 
 bool UFSMComponent::IsRunning() const
